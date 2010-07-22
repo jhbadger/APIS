88e00a54d010209f2802f926f8ae75c32634798d
Jonathan Badger <jhbadger@gmail.com>
2010-07-22 13:28:05 -0700

    createApisCSV now uses DataMapper, additional models for ComboDB

--- ApisDB.rb
+++ ApisDB.rb
@@ -13,21 +13,30 @@ class Sequence
   include DataMapper::Resource
   storage_names[:default] = "sequence"
   belongs_to :dataset
-  has n, :alignments, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
-  has n, :annotations, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
-  has n, :blasts, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
-  has 1, :classification, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
-  has 1, :tree, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
+  has n, :alignments, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
+  has n, :annotations, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
+  has n, :blasts, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
+  has 1, :classification, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
+  has 1, :tree, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
   property :dataset_id, String, :field => "dataset", :length => 50, :key => true, :index => true
-  property :seq_name, String, :length => 100, :key => true, :index => true
+  property :name, String, :field => "seq_name", :length => 100, :key => true, :index => true
   property :sequence, Text, :lazy => false
   property :processed, Boolean, :default => 0
+  def cog
+    if (tree.nil?)
+      return ""
+    else
+      homolog = tree.homolog
+      cogs = DataMapper.repository(:combodb) {homolog.blastmatches.cogs}
+      return cogs.first.to_s
+    end
+  end
 end
 
 class Alignment
   include DataMapper::Resource
   storage_names[:default] = "alignment"
-  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
+  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
   property :dataset_id, String, :field => "dataset", :length => 50, :key => true
   property :seq_name, String, :length => 100, :key => true
   property :alignment_name, String, :length => 100
@@ -38,17 +47,20 @@ end
 class Annotation
   include DataMapper::Resource
   storage_names[:default] = "annotation"
-  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
+  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
   property :dataset_id, String, :field => "dataset", :length => 50, :key => true
   property :seq_name, String, :length => 100, :key => true
   property :annotation, String, :length => 10000
   property :source, String, :length => 100, :key => true
+  def to_s
+    return annotation
+  end
 end
 
 class Blast
   include DataMapper::Resource
   storage_names[:default] = "blast"
-  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
+  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
   property :dataset_id, String, :field => "dataset", :length => 50, :key => true
   property :seq_name, String, :length => 100, :key => true
   property :subject_name, String, :length => 100, :key => true
@@ -67,14 +79,14 @@ end
 class Classification
   include DataMapper::Resource
   storage_names[:default] = "classification"
-  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
+  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
   property :dataset_id, String, :field => "dataset", :length => 50, :key => true
   property :seq_name, String, :length => 100, :key => true
   property :kingdom, String, :length => 50, :index => true
   property :kingdom_outgroup, Boolean, :index => true
   property :phylum, String, :length => 50, :index => true
   property :phylum_outgroup, Boolean, :index => true
-  property :class, String, :length => 50, :index => true
+  property :classname, String, :field => "class", :length => 50, :index => true
   property :class_outgroup, Boolean, :index => true
   property :order, String, :field => "ord", :length => 50, :index => true
   property :order_outgroup, Boolean, :field => "ord_outgroup", :index => true
@@ -84,13 +96,28 @@ class Classification
   property :genus_outgroup, Boolean, :index => true
   property :species, String, :length => 50, :index => true
   property :species_outgroup, Boolean, :index => true
+  def to_s
+    s = ""
+    [kingdom, phylum, classname, order, family, genus, species].each do |level|
+      s += "; " if (s != "")
+      s += level if (level != "Mixed" && level != "Undefined")
+    end
+    return s    
+  end
 end
 
 class Tree
   include DataMapper::Resource
   storage_names[:default] = "tree"
-  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :seq_name]
+  belongs_to :sequence, :child_key=>[:dataset_id, :seq_name], :parent_key=>[:dataset_id, :name]
   property :dataset_id, String, :field => "dataset", :length => 50, :key => true
   property :seq_name, String, :length => 100, :key => true
   property :tree, Text, :lazy => false
+  def to_s
+    return tree.chomp
+  end
+  def homolog
+    pname = NewickTree.new(tree).relatives(seq_name).first.first.split("__").first
+    return DataMapper.repository(:combodb) {Protein.first(:name => pname)}
+  end
 end
---
+++ ComboDB-metadata.rb
@@ -0,0 +1,37 @@
+class Protein
+  has n, :blastmatches, :child_key=>[:name], :parent_key=>[:name]
+end
+
+class Blastmatch
+  include DataMapper::Resource
+  belongs_to :protein, :child_key=>[:name], :parent_key=>[:name]
+  has n, :cogs, :child_key=>[:protein], :parent_key=>[:hit]
+  storage_names[:combodb] = "blast"
+  property :source, String, :length=>100, :key=>true, :index=>true
+  property :target, String, :length=>100, :key=>true, :index=>true
+  property :blast, String, :length=>100,  :key=>true, :index=>true
+  property :name, String, :field=> "query", :length=>100, :key=>true, :index=>true
+  property :hit, String, :length=>100, :key=>true, :index=>true
+  property :evalue, Float
+end
+
+class Cog
+  include DataMapper::Resource
+  storage_names[:combodb] = "cog_proteins"
+  belongs_to :blastmatch, :child_key=>[:protein], :parent_key=>[:hit]
+  has 1, :cogdef, :child_key=>[:cog_name], :parent_key=>[:name]
+  property :protein, String, :length=>100, :key=>true
+  property :species, String, :length=>100
+  property :name, String, :field=>"cog", :length=>100
+  def to_s
+    return name + " " + cogdef.definition
+  end
+end
+
+class Cogdef
+  include DataMapper::Resource
+  storage_names[:combodb] = "cog_definitions"
+  belongs_to :cog, :child_key=>[:cog_name], :parent_key=>[:name]
+  property :cog_name, String, :field=> "cog", :length=>100, :key=> true
+  property :definition, Text, :lazy => false
+end
