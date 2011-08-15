02825370b4de557c3c4d6b72e447535680b4912c
Jonathan Badger <jhbadger@gmail.com>
2011-08-14 22:30:33 -0400

    pipe postprocessing for SunGrid jobs

--- SunGrid.rb
+++ SunGrid.rb
@@ -1,71 +1,69 @@
 # class to encapsulate running jobs on a Sun Grid Engine
 class SunGrid
   attr :subdir
-  def initialize(command, project = nil, memory = nil, queue = "default", dir = Dir.pwd + "/.tmp")
-    @command = command
-    @name = File.basename(@command).split(" ").first
+  def initialize(command, project = nil, memory = nil, queue = nil)
+    @command = [command].flatten
+    @name = @command.first.gsub("/","_").split(" ").first
     @project = project
-    @dir = dir
-    @subdir = dir + "/" + Time.now.to_i.to_s
     @memory = memory
     @queue = queue
     @count = 0
-    if (!File.exists?(@dir))
-      begin
-        Dir.mkdir(@dir)
-      rescue
-        STDERR << "Cannot create needed tmp directory: " << @dir << "\n"
-        exit(1)
-      end
-    end
-    if (!File.exists?(@subdir))
-      begin
-        Dir.mkdir(@subdir)
-      rescue
-        STDERR << "Cannot create needed tmp directory: " << @subdir << "\n"
-        exit(1)
-      end
-    end
   end
   
   # return name for next data file
   def next
     @count += 1
-    filename = @subdir + "/" + @name + "_input." + @count.to_s
+    filename = @name + "_input." + @count.to_s
     return filename
   end
   
-  # write job script to tmp dir
+  # write job script
   def writeJob
-    out = File.new(@subdir + "/" + @name + ".com", "w")
+    out = File.new(@name + ".com", "w")
     out.printf("\#!/bin/bash\n")
-    out.printf("\#$ -S /bin/bash\n")
-    out.printf("\#$ -o %s/output.$TASK_ID.stdout\n", @subdir)
-    out.printf("\#$ -e %s/output.$TASK_ID.stderr\n", @subdir)
-    out.printf(". /etc/profile\n")
-    out.printf("module add dot\n")
-    out.printf("cd %s\n", @subdir)
-    out.printf("%s %s_input.$SGE_TASK_ID\n", @command, @name)
-    out.close
-  end
-  
-  # remove stuff from tmp dir
-  def cleanup
-    Dir.foreach(@subdir).each do |file|
-      next if file == "." || file == ".."
-      File.unlink(@subdir + "/" +file)
+    if (@command[1])
+      out.printf("%s %s_input.$SGE_TASK_ID | %s\n", @command.first, 
+      @name, @command[1])
+    else
+      out.printf("%s %s_input.$SGE_TASK_ID\n", @command.first, @name)
     end
-    Dir.rmdir(@subdir)
+    out.close
+    File.chmod(0777, @name + ".com")
   end
   
   # submit array to grid
-  def submit
+  def submit(sync = false)
     writeJob
     qsub = "qsub -t 1:" + @count.to_s
+    qsub += " -sync yes" if (sync)
     qsub += " -P #{@project}" if @project
-    qsub += " -l #{@memory}" if (@memory)
-    qsub += " -l #{@queue}" if (@queue != "default")
-    qsub += " #{@name}.com"
-    p qsub
+    qsub +=  " -l memory=#{@memory}" if (@memory)
+    qsub += " -l queue=#{@queue}" if (@queue)
+    qsub += " ./#{@name}.com"
+    system(qsub)
+  end
+
+  # concatenate output, error, removing input files
+  def cleanup
+    Dir.glob(@name + "_input*").each do |file|
+      File.unlink(file)
+    end
+    out = File.new(@name + ".out", "w")
+    Dir.glob(@name + ".com.o*").each do |file|
+      File.new(file).each do |line|
+        out.print line
+      end
+      File.unlink(file)
+    end
+    out.close
+    err = File.new(@name + ".err", "w")
+    Dir.glob(@name + ".com.e*").each do |file|
+      File.new(file).each do |line|
+        err.print line
+      end
+      File.unlink(file)
+    end
+    err.close
+    File.unlink(@name + ".com")
   end
 end
